b748f69ee1289ffe6ddee605745ffb0ef358e4cb
saviour159 <khushpatel1222@gmail.com>
2026-06-10 19:22:48 +0530

    Phase 6 - Teams Data

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,7 +1,23 @@
+import { teams } from "@/data/teams";
+
 export default function TeamsPage() {
   return (
-    <div className="min-h-screen bg-[#021526] text-white p-10">
-      <h1 className="text-5xl font-bold">Teams</h1>
-    </div>
+    <main className="min-h-screen bg-[#021526] text-white p-10">
+      <h1 className="text-5xl font-bold mb-10">Teams</h1>
+
+      <div className="grid md:grid-cols-3 gap-6">
+        {teams.map((team) => (
+          <div
+            key={team.id}
+            className="rounded-3xl border border-white/10 bg-white/5 p-8">
+            <h2 className="text-3xl font-bold">{team.name}</h2>
+
+            <p className="mt-2">FIFA Rank: {team.fifaRank}</p>
+
+            <p>{team.confederation}</p>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
---
+++ data/teams.ts
@@ -0,0 +1,32 @@
+export const teams = [
+  {
+    id: 1,
+    name: "Argentina",
+    fifaRank: 1,
+    confederation: "CONMEBOL",
+  },
+  {
+    id: 2,
+    name: "Brazil",
+    fifaRank: 5,
+    confederation: "CONMEBOL",
+  },
+  {
+    id: 3,
+    name: "Spain",
+    fifaRank: 3,
+    confederation: "UEFA",
+  },
+  {
+    id: 4,
+    name: "France",
+    fifaRank: 2,
+    confederation: "UEFA",
+  },
+  {
+    id: 5,
+    name: "England",
+    fifaRank: 4,
+    confederation: "UEFA",
+  },
+];
